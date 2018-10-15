c0b3b928b955eee0c1c7261d6565fda3c2e13b3b
starlord <starlord@xhiftcorp.com>
2018-10-15 14:11:00 +0900

    model 수정중

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampedModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
+
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User, on_delete = models.PROTECT)
