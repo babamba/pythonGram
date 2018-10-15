33165ec71d8b89f94fbdbd1b7c52acd8980db347
starlord <starlord@xhiftcorp.com>
2018-10-15 15:25:50 +0900

    admin model 추가 및 컬럼 추가

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -4,6 +4,8 @@ from nomadgram.users import models as user_models
 # Create your models here.
 class TimeStampedModel(models.Model):
 
+    # 생성 및 업데이트 시 공통적으로 사용되는 타임스탬프 정보 추상
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -13,15 +15,34 @@ class TimeStampedModel(models.Model):
 
 class Image(TimeStampedModel):
 
+    # 이미지 모델 ( 타임스탬프 클래스 상속 )
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+
+    def  __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
 
 class Comment(TimeStampedModel):
 
+    # 댓글 모델 ( 타임스탬프 클래스 상속 )
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True,)
+
+    def  __str__(self):
+        return self.message
 
 class Like(TimeStampedModel):
 
-    creator = models.ForeignKey(user_models.User, on_delete = models.PROTECT)
+    # 좋아요 ( relationship) 모델 ( 타임스탬프 클래스 상속 )
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+    def  __str__(self):
+        return 'User : {} - Image Caption : {}'.format(self.creator.username, self.image.caption)
--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -23,5 +23,12 @@ class User(AbstractUser):
     phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
 
+    # A가 B를 팔로잉을 하면 B의 팔로워가 되어야 
+    # 하는데 B의 프로필을 보면 같은 팔로잉이 되어있는 상황  symmetrical=False,  related_name="user"
+    # 이와같이 해결해야하는 것 같음. 
+    
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
+
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
