a06e43d027985b25c194bc8d82759daf52d6a03f
starlord <starlord@xhiftcorp.com>
2018-10-16 17:20:14 +0900

    좋아요 기능 개선 및 HTTP STATUS값 수정

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -26,12 +26,46 @@ from . import views
 
 app_name = "images"
 
-# like쪽에 int:image_id는 스프링 requestParam처럼 넘어오는 쿼리스트링 사용하도록 받는거 
+# like쪽에 int:image_id는 스프링 requestParam처럼 넘어오는 쿼리스트링 사용하도록 받는거 대신 views.py에서 파라미터로 지정안하면 에러 발생
+
+# 2.0 이하 url(r'^articles/(?P<year>[0-9]{4})/$', views.year_archive),
+# 2.0 부터 path('articles/<int:year>', views.year_archive),
 
 urlpatterns = [
     path("feed/", view=views.Feed.as_view(), name="feed"),
     path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image")
 ]
+
+# 장고 업데이트로 인한 url path의 정규식사용
+# converter의 종류는
+
+# str: /를 제외한 모든 문자열
+# int
+# slug: hypen, undersocre로 연결된 slug 형태의 str
+# uuid
+# path
+
+# 출처: http://seulcode.tistory.com/210 [seulcoding]
+
+# class FourDigitYearConverter: 
+#     regex = '[0-9]{4}' 
+#     def to_python(self, value): 
+#         return int(value) 
+        
+#     def to_url(self, value): 
+#         return '%04d' % value
+
+# 위에 import 처리 from django.urls import register_converter, path
+#from . import converters, views # 이런식으로 등록한다. 
+# 록한 converter를 호출해서 사용하면 됨 
+# register_converter(converters.FourDigitYearConverter, 'yyyy') 
+# 
+# urlpatterns = [ 
+#   path('articles/2003/', views.special_case_2003), 
+#   path('articles/<yyyy:year>/', views.year_archive), ... 
+# ]
+
+# 그외 전 버전으로 쓰려면 re_path로 처리
 
 # /images/3/like/
 
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
@@ -31,9 +32,35 @@ def get_key(image):
     return image.created_at
 
 
-
 class LikeImage(APIView):
 
     def get(self, request, image_id ,format=None):
-        print(image_id);
-        return Response(status=200)
+        
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator = user,
+                image= found_image
+            )
+            print("exist_like & delete - name : " , user , " / image : " , found_image )
+            preexisting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+            print(found_image)
+
+            new_like = models.Like.objects.create(
+                creator = user,
+                image = found_image
+            )
+            print("new_like & save - name : " , user , " / image : " , found_image )
+            new_like.save()
+
+        return Response(status=status.HTTP_201_CREATED)
