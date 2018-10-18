4f5eb20a26e67e3936003bd182bd637a6b666434
starlord <starlord@xhiftcorp.com>
2018-10-18 18:40:26 +0900

    프로필 이미지 수정

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -19,7 +19,7 @@ class ListUserSerializer(serializers.ModelSerializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
-    images = images_serializers.CountImageSerializer(many=True)
+    images = images_serializers.CountImageSerializer(many=True, read_only=True)
     # ReadOnlyField 는 ㅎ당 필드들을 수정하지않는다.
     post_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
