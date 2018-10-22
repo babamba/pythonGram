f1cb5a7db556c6889bb4f90b10199131d91700a5
starlord <starlord@xhiftcorp.com>
2018-10-22 15:15:55 +0900

    reducer 기본 틀 및 소스 패스 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.scss';
+import styles from 'App.scss';
 
 fetch('/notifications/')
 
@@ -9,20 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles.App__header}>
-          <img src={logo} className={styles.App__logo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles.App__link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
